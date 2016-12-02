98627ebc6fc4d7c73d8089dc6c17d3c5a8d5a731
renatodasilvaborges <renatodasilvaborges@gmail.com>
2016-12-02 00:10:07 -0200

    Update T25.c

--- T25.c
+++ T25.c
@@ -7,44 +7,44 @@ int tamString(char *o) {// função responsável por contar o tamanho de uma Str
     return (i);
 }
 
-void verificaArquivos(int argc){// Função para validar se a aplicação recebeu arquivo. 
-	if(argc >= 2){
+void verificaArquivos(int argc){		// Função para validar se a aplicação recebeu arquivo. 
+	if(argc >= 2){ 				// se o parâmetro for >= 2, quer dizer que na execução do código, foi enviado um arquivo
 		printf("#OK [Arquivos enviados] : \n");
 	} else {
-		printf("#ERRO [Nao foi enviado arquivos]"); 
-		exit(0);
+		printf("#ERRO [Nao foi enviado arquivos]");  // se for < 2, quer dizer que não foi enviado nenhum arquivo na execução do programa
+		exit(0); // finaliza
 	}
 }
 
-int validaTxt(char *nome){ // Função responsável por validar se o arquivo é texto ou não
-    int t = tamString(nome);
-    t = t - 4; 
-    if ((nome[t]=='.')&&(nome[t+1]=='t')&&(nome[t+2]=='x')&&(nome[t+3]=='t')) {
-        return(1);
+int validaTxt(char *nome){ 			// Função responsável por validar se o arquivo é texto ou não
+    int t = tamString(nome); 			// usa a função responsável por retornar o tamanho da string. 
+    t = t - 4; 					// Pega o tamanho da string e volta 4 "casas" :) 
+    if ((nome[t]=='.')&&(nome[t+1]=='t')&&(nome[t+2]=='x')&&(nome[t+3]=='t')) { // identifica se as próximas 4 casas será .txt
+        return(1); 				// se for txt retorna 1
     }
     else {
-        return(0);
+        return(0); // se não for txt retorna 0
     }
 }
 
 int countMaiuscula(FILE *f){ // Função responsável por avaliar a quantidade de letras maiusculas dentro do arquivo
     int count = 0, t = 0;
     char let;
-    t = fseek(f, 0, SEEK_SET);
-    for(let = fgetc(f);!feof(f);let=fgetc(f)){
-        if((let>='A')&&(let<='Z')) {
-            count++;
+    t = fseek(f, 0, SEEK_SET); 			// vai pra posição inicial do arquivo
+    for(let = fgetc(f);!feof(f);let=fgetc(f)){  // enquanto a letra avaliada não !feof, executa 
+        if((let>='A')&&(let<='Z')) { 		// valiando se é maiscula 
+            count++; 				// se for maiscula a letra, contar + 1
         }
     }
-    return(count);
+    return(count); // retorna o valor
 }
 
 int countMinusculo(FILE *f){ // Função responsável por avaliar a quantidade de letras minusculas dentro do arquivo
     int count = 0, t = 0;
     char let;
-    t = fseek(f, 0, SEEK_SET);
-    for(let = fgetc(f);!feof(f);let=fgetc(f)){
-        if((let>='a')&&(let<='z')) {
+    t = fseek(f, 0, SEEK_SET); 			// vai pra posição inicial do arquivo
+    for(let = fgetc(f);!feof(f);let=fgetc(f)){  // valida enquanto o arquivo for diferente de feof
+        if((let>='a')&&(let<='z')) { 		// valida caracteres minusculos 
             count++;
         }
     }
@@ -52,13 +52,13 @@ int countMinusculo(FILE *f){ // Função responsável por avaliar a quantidade d
 }
 
 void verificaUnder(FILE *f,char *nome){ // Função responsável por verificar se o nome do arquivo começa com _
-    int t;
+    int t;				
     char let;
-    if(nome[0] == '_') {
-	    t=fseek(f, -1, SEEK_SET);// Vai para o inicio do arquivo
+    if(nome[0] == '_') {		// valida se a primeira letra do arquivo é _
+	    t=fseek(f, -1, SEEK_SET);  // Vai para o inicio do arquivo
 	    for(let = fgetc(f);!feof(f);let=fgetc(f)){
 	        if ((let >= 'a')&&(let<='z')){
-	            let=let-32;
+	            let=let-32; // para tornar a váriavel maiscula, é utilizado a manipulação da tabela ASC
 	            fseek(f, 0, SEEK_CUR);
 	            fprintf(f,"%c", let);
 	        }
@@ -94,9 +94,17 @@ int main(int argc, char *argv[]){
 			countBytesNaoTxt = countBytesNaoTxt + ftell(f); // Somando o total de bytes de arquivos não txt 
 		}
 		countTotalBytes = countTotalBytes + ftell(f);
-		fclose(f);
+		fclose(f); // por questões de segurança, após manipulado o arquivo, o mesmo deve ser encerrado. 
 		i++; 
 	}
 	relGeral(totalNaoTxt, countBytesNaoTxt, totalMinusculo, totalMaiusculo, countTotalBytes);
 	return 0; 
 }
+
+
+// a função fseek, é responsável por movimentar a posição onde o arquivo se encontra
+// o final de um arquivo, pode ser identificado pelo retorno feof, utilizado aqui em algumas funções
+// 
+
+
+
